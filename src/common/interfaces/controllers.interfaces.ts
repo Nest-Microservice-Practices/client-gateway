dce86db6af1041e5e9c7d0533e623599082295d7
NoinBlake336 <josechocobar336@gmail.com>
2025-03-20 23:38:48 -0300

    implementation of interfaces in orders controllers

--- src/common/interfaces/controllers.interfaces.ts
+++ src/common/interfaces/controllers.interfaces.ts
@@ -1,6 +1,9 @@
 import { CreateProductDto } from 'src/products/dto/create-product.dto';
 import { UpdateProductDto } from 'src/products/dto/update-product.dto';
 import { PaginationDto } from '../dto/pagination.dto';
+import { CreateOrderDto } from '../../orders/dto/create-order.dto';
+import { OrderPaginationDto } from '../../orders/dto/order.pagination.dto';
+import { StatusDto } from '../../orders/dto/status.dto';
 
 export interface IProductsController {
   createProduct(createProductDto: CreateProductDto);
@@ -8,4 +11,12 @@ export interface IProductsController {
   findOneProduct(id: number);
   deleteProduct(id: number);
   updateProduct(id: number, updateProductDto: UpdateProductDto);
+}
+
+export interface IOrdersController {
+  createOrder(createOrderDto: CreateOrderDto);
+  findAllOrder(orderPaginationDto: OrderPaginationDto);
+  findOneOrder(id: string);
+  findOrderByStatus(statusDto: StatusDto, paginationDto: PaginationDto);
+  updateStatusOrder(id: string, statusDto: StatusDto);
 }
--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -13,26 +13,26 @@ import { ORDER_SERVICE } from 'src/config';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { CreateOrderDto, OrderPaginationDto, StatusDto } from './dto';
 import { firstValueFrom } from 'rxjs';
-import { PaginationDto } from 'src/common';
+import { IOrdersController, PaginationDto } from 'src/common';
 
 @Controller('orders')
-export class OrdersController {
+export class OrdersController implements IOrdersController {
   constructor(
     @Inject(ORDER_SERVICE) private readonly orderClient: ClientProxy,
   ) {}
 
   @Post()
-  create(@Body() createOrderDto: CreateOrderDto) {
+  createOrder(@Body() createOrderDto: CreateOrderDto) {
     return this.orderClient.send({ cmd: 'create_order' }, createOrderDto);
   }
 
   @Get()
-  findAll(@Query() orderPaginationDto: OrderPaginationDto) {
+  findAllOrder(@Query() orderPaginationDto: OrderPaginationDto) {
     return this.orderClient.send({ cmd: 'find_all_order' }, orderPaginationDto);
   }
 
   @Get('id/:id')
-  async findOne(@Param('id', ParseUUIDPipe) id: string) {
+  async findOneOrder(@Param('id', ParseUUIDPipe) id: string) {
     try {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       const order = await firstValueFrom(
@@ -45,7 +45,7 @@ export class OrdersController {
   }
 
   @Get(':status')
-  async findOneByStatus(
+  async findOrderByStatus(
     @Param() statusDto: StatusDto,
     @Query() paginationDto: PaginationDto,
   ) {
